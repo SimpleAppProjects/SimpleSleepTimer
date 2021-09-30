1699c41d214742b6f8a08457a73448cc969c8031
Dave Antoine <bryan2894@gmail.com>
2021-09-30 14:21:12 -0400

    TimerService: add ongoing activity for local timer

--- wear/src/main/java/com/thewizrd/simplesleeptimer/services/TimerService.kt
+++ wear/src/main/java/com/thewizrd/simplesleeptimer/services/TimerService.kt
@@ -9,6 +9,12 @@ import android.text.format.DateUtils
 import android.view.KeyEvent
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.LocusIdCompat
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.content.pm.ShortcutManagerCompat
+import androidx.core.graphics.drawable.IconCompat
+import androidx.wear.ongoing.OngoingActivity
+import androidx.wear.ongoing.Status
 import com.thewizrd.shared_resources.services.BaseTimerService
 import com.thewizrd.shared_resources.sleeptimer.TimerModel
 import com.thewizrd.shared_resources.utils.TimerStringFormatter
@@ -21,6 +27,7 @@ class TimerService : BaseTimerService() {
     companion object {
         const val NOT_CHANNEL_ID = "SimpleSleepTimer.timerservice"
         const val NOTIFICATION_ID = 1000
+        private const val LOCAL_TIMER_LOCUS_ID = "local_timer"
     }
 
     override val notificationId: Int
@@ -35,58 +42,93 @@ class TimerService : BaseTimerService() {
     override fun updateTimerNotification(model: TimerModel): Notification {
         val remainingTime = model.remainingTimeInMs
 
-        return NotificationCompat.Builder(this, NOT_CHANNEL_ID)
+        val notifBuilder = NotificationCompat.Builder(this, NOT_CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_hourglass_empty)
             //.setContentTitle(getString(R.string.title_sleeptimer))
             .setColor(ContextCompat.getColor(this, R.color.colorPrimary))
+            .setColorized(true)
             .setOngoing(true)
             .setOnlyAlertOnce(true)
             .setShowWhen(false)
             .setSound(null)
-            .addAction(
-                0,
-                getString(android.R.string.cancel),
-                    getCancelIntent(this)
-                )
-                .setContentIntent(getClickIntent(this))
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setCategory(NotificationCompat.CATEGORY_ALARM)
-                .apply {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        setUsesChronometer(true)
-                        setChronometerCountDown(true)
-                        setWhen(model.endTimeInMs)
-                    } else {
-                        setContentText(
-                            TimerStringFormatter.formatTimeRemaining(
-                                this@TimerService, remainingTime
-                            )
+            .addAction(0, getString(android.R.string.cancel), getCancelIntent(this))
+            .setContentIntent(getClickIntent(this))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setCategory(NotificationCompat.CATEGORY_ALARM)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setLocusId(LocusIdCompat(LOCAL_TIMER_LOCUS_ID))
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    setUsesChronometer(true)
+                    setChronometerCountDown(true)
+                    setWhen(model.endTimeInMs)
+                } else {
+                    setContentText(
+                        TimerStringFormatter.formatTimeRemaining(
+                            this@TimerService, remainingTime
                         )
-                    }
-
-                    val remainingMinsMs =
-                        model.remainingTimeInMs - (model.remainingTimeInMs % DateUtils.MINUTE_IN_MILLIS)
-                    if (remainingMinsMs < (TimerModel.MAX_TIME_IN_MINS - 1).times(DateUtils.MINUTE_IN_MILLIS)) {
-                        addAction(
-                            0,
-                            "+" + TimerStringFormatter.getNumberFormattedQuantityString(
-                                this@TimerService, R.plurals.minutes_short, 1
-                            ),
-                            getExtend1MinPendingIntent()
-                        )
-                    }
-                    if (remainingMinsMs < (TimerModel.MAX_TIME_IN_MINS - 5).times(DateUtils.MINUTE_IN_MILLIS)) {
-                        addAction(
-                            0,
-                            "+" + TimerStringFormatter.getNumberFormattedQuantityString(
-                                this@TimerService, R.plurals.minutes_short, 5
-                            ),
-                            getExtend5MinPendingIntent()
-                        )
-                    }
+                    )
                 }
-            //.setStyle(NotificationCompat.DecoratedCustomViewStyle())
+
+                val remainingMinsMs =
+                    model.remainingTimeInMs - (model.remainingTimeInMs % DateUtils.MINUTE_IN_MILLIS)
+                if (remainingMinsMs < (TimerModel.MAX_TIME_IN_MINS - 1).times(DateUtils.MINUTE_IN_MILLIS)) {
+                    addAction(
+                        0,
+                        "+" + TimerStringFormatter.getNumberFormattedQuantityString(
+                            this@TimerService, R.plurals.minutes_short, 1
+                        ),
+                        getExtend1MinPendingIntent()
+                    )
+                }
+                if (remainingMinsMs < (TimerModel.MAX_TIME_IN_MINS - 5).times(DateUtils.MINUTE_IN_MILLIS)) {
+                    addAction(
+                        0,
+                        "+" + TimerStringFormatter.getNumberFormattedQuantityString(
+                            this@TimerService, R.plurals.minutes_short, 5
+                        ),
+                        getExtend5MinPendingIntent()
+                    )
+                }
+            }
+
+        if (model.isRunning) {
+            val ongoingActivityStatus = Status.forPart(
+                Status.TimerPart(SystemClock.elapsedRealtime() + model.remainingTimeInMs)
+            )
+
+            val ongoingActivity =
+                OngoingActivity.Builder(applicationContext, NOTIFICATION_ID, notifBuilder)
+                    .setStaticIcon(R.drawable.ic_hourglass_empty)
+                    .setTitle(getString(R.string.title_sleeptimer))
+                    .setStatus(ongoingActivityStatus)
+                    .setLocusId(LocusIdCompat(LOCAL_TIMER_LOCUS_ID))
+                    .build()
+
+            ongoingActivity.apply(applicationContext)
+        }
+
+        return notifBuilder.build()
+    }
+
+    private fun createTimerShortcut() {
+        val shortcut = ShortcutInfoCompat.Builder(this, LOCAL_TIMER_LOCUS_ID)
+            .setShortLabel(getString(R.string.title_sleeptimer))
+            .setIcon(IconCompat.createWithResource(this, R.drawable.ic_hourglass_empty))
+            .setIntent(
+                Intent(this, getOnClickActivityClass())
+                    .setAction(Intent.ACTION_VIEW)
+            )
+            .setLocusId(LocusIdCompat(LOCAL_TIMER_LOCUS_ID))
             .build()
+
+        ShortcutManagerCompat.pushDynamicShortcut(this, shortcut)
+    }
+
+    override fun sendTimerStarted() {
+        super.sendTimerStarted()
+        createTimerShortcut()
     }
 
     override fun getOnClickActivityClass(): Class<out Activity> {
@@ -94,6 +136,11 @@ class TimerService : BaseTimerService() {
     }
 
     override fun sendPublicTimerUpdate() {}
+
+    override fun sendTimerCancelled() {
+        super.sendTimerCancelled()
+        ShortcutManagerCompat.removeDynamicShortcuts(this, listOf(LOCAL_TIMER_LOCUS_ID))
+    }
 
     override fun pauseSelectedMediaPlayer() {
         // Check if audio player preference exists
