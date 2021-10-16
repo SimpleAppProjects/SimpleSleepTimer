4f1bd4a55d4f1ad63fd973bcb9b6f4ea9053130c
Dave Antoine <bryan2894@gmail.com>
2021-10-15 22:41:18 -0400

    WearableListenerActivity: fix show app store listing action
    * Also remove unnecessary additional coroutines (we're already on the main thread)

--- wear/src/main/java/com/thewizrd/simplesleeptimer/WearableListenerActivity.kt
+++ wear/src/main/java/com/thewizrd/simplesleeptimer/WearableListenerActivity.kt
@@ -19,6 +19,9 @@ import com.google.android.gms.wearable.MessageClient.OnMessageReceivedListener
 import com.thewizrd.shared_resources.helpers.WearConnectionStatus
 import com.thewizrd.shared_resources.helpers.WearableHelper
 import com.thewizrd.simplesleeptimer.activities.AppCompatLiteActivity
+import com.thewizrd.simplesleeptimer.helpers.showConfirmationOverlay
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.guava.await
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
@@ -96,38 +99,43 @@ abstract class WearableListenerActivity : AppCompatLiteActivity(), OnMessageRece
             connect()
 
             if (mPhoneNodeWithApp == null) {
-                lifecycleScope.launch {
-                    Toast.makeText(
-                        this@WearableListenerActivity,
-                        "Device is not connected or app is not installed on device...",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+                Toast.makeText(
+                    this@WearableListenerActivity,
+                    "Device is not connected or app is not installed on device...",
+                    Toast.LENGTH_SHORT
+                ).show()
 
                 when (PhoneTypeHelper.getPhoneDeviceType(this@WearableListenerActivity)) {
                     PhoneTypeHelper.DEVICE_TYPE_ANDROID -> {
-                        LocalBroadcastManager.getInstance(this@WearableListenerActivity)
-                            .sendBroadcast(
-                                Intent(ACTION_SHOWSTORELISTING)
-                            )
+                        // Open store on remote device
+                        val intentAndroid = Intent(Intent.ACTION_VIEW)
+                            .addCategory(Intent.CATEGORY_BROWSABLE)
+                            .setData(WearableHelper.getPlayStoreURI())
+
+                        runCatching {
+                            remoteActivityHelper.startRemoteActivity(intentAndroid)
+                                .await()
+
+                            showConfirmationOverlay(true)
+                        }.onFailure {
+                            if (it !is CancellationException) {
+                                showConfirmationOverlay(false)
+                            }
+                        }
                     }
                     PhoneTypeHelper.DEVICE_TYPE_IOS -> {
-                        lifecycleScope.launch {
-                            Toast.makeText(
-                                this@WearableListenerActivity,
-                                "Connected device is not supported",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
+                        Toast.makeText(
+                            this@WearableListenerActivity,
+                            "Connected device is not supported",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                     else -> {
-                        lifecycleScope.launch {
-                            Toast.makeText(
-                                this@WearableListenerActivity,
-                                "Connected device is not supported",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
+                        Toast.makeText(
+                            this@WearableListenerActivity,
+                            "Connected device is not supported",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
             } else {
